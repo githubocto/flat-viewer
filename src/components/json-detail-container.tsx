b3d7505012d851192893843351fbbadcf80266ab
Matt Rothenberg <mattrothenberg@github.com>
2021-03-12 10:27:42 -0500

    fix: uncontrolled input warning from downshift

--- src/components/json-detail-container.tsx
+++ src/components/json-detail-container.tsx
@@ -68,11 +68,13 @@ export function JSONDetailContainer(props: JSONDetailContainerProps) {
         <React.Fragment>
           <div className="p-4 bg-white border-b border-gray-200">
             <div className="flex items-center space-x-2">
-              <SHAPicker
-                onChange={setSelectedSha}
-                value={selectedSha}
-                commits={data}
-              />
+              {selectedSha && (
+                <SHAPicker
+                  onChange={setSelectedSha}
+                  value={selectedSha}
+                  commits={data}
+                />
+              )}
             </div>
           </div>
           <div className="flex-1 overflow-hidden">
