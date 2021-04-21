34ce4b8bd7f7203f2f08fc4b559a68d37f20b24b
Amelia Wattenbeger <wattenberger@github.com>
2021-04-21 16:57:52 -0400

    add a title & description

--- src/components/repo-detail.tsx
+++ src/components/repo-detail.tsx
@@ -31,7 +31,7 @@ export function RepoDetail(props: RepoDetailProps) {
   const [filename, setFilename] = useQueryParam("filename", StringParam);
   const [selectedSha, setSelectedSha] = useQueryParam("sha", StringParam);
 
-  const { data: files } = useGetFiles(
+  const { data: files, status: filesStatus } = useGetFiles(
     { owner, name },
     {
       onSuccess: (data) => {
@@ -93,131 +93,139 @@ export function RepoDetail(props: RepoDetailProps) {
         {owner}/{name} – Flat
       </Title>
       <Toaster position="bottom-left" />
-      <div className="bg-indigo-600 lg:flex p-4 space-y-4 lg:space-y-0 lg:space-x-4">
-        <div className="space-y-2">
-          <p className="text-xs font-medium text-indigo-200">Repository</p>
-          <div className="font-mono text-sm text-white">
-            <a
-              className="hover:underline focus:underline bg-indigo-700 hover:bg-indigo-800 focus:bg-indigo-800 h-9 rounded text-white inline-flex items-center px-2 focus:outline-none focus:ring-2 focus:ring-indigo-400"
-              target="_blank"
-              rel="noopener noreferrer"
-              href={repoUrl}
-            >
-              <div className="flex items-center space-x-2">
-                <RepoIcon />
-                <div
-                  className="overflow-ellipsis whitespace-nowrap overflow-hidden text-xs"
-                  style={{
-                    maxWidth: "min(30em, 100% - 1em)",
-                  }}
-                >
-                  {owner}/{name}
-                </div>
-                <div className="opacity-50">
-                  <LinkExternalIcon />
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        {!!(files || []).length && (
-          <div className="space-y-2">
-            <p className="text-xs font-medium text-indigo-200">Data File</p>
-            <FilePicker
-              value={filename || ""}
-              placeholder="Select a file"
-              onChange={(newFilename) => {
-                setFilename(newFilename);
-              }}
-              items={files || []}
-              itemRenderer={(item) => (
-                <span className="font-mono text-xs">{item}</span>
-              )}
-            />
-          </div>
-        )}
 
-        {Boolean(filename) && (
+      <div className="p-4 bg-indigo-600">
+        <div className="pb-3 text-indigo-100 font-light text-sm">
+          <strong className="font-bold">Flat Viewer</strong> a simple tool for
+          exploring flat data files in GitHub repositories.
+        </div>
+        <div className="lg:flex space-y-4 lg:space-y-0 lg:space-x-4">
           <div className="space-y-2">
-            <p className="text-xs font-medium text-indigo-200">Commit</p>
-            {commits && (
-              <Picker<string>
-                label="Choose a commit"
-                placeholder="Select a SHA"
-                onChange={setSelectedSha}
-                value={selectedSha || ""}
-                items={commits.map((commit) => commit.sha)}
-                disclosureClass="appearance-none bg-indigo-700 hover:bg-indigo-800 focus:bg-indigo-800 h-9 px-2 rounded text-white text-xs focus:outline-none focus:ring-2 focus:ring-indigo-400 w-full lg:max-w-md"
-                itemRenderer={(sha) => {
-                  const commit = commits.find((commit) => commit.sha === sha);
-                  return (
-                    <div className="flex flex-col space-y-1 text-xs">
-                      <DisplayCommit
-                        message={commit?.commit.message}
-                        author={commit?.commit.author?.email}
-                        filename={filename}
-                      />
-                      <div className="flex items-center space-x-2">
-                        <div className="flex items-center space-x-2">
-                          <p className="text-gray-600">
-                            {formatDistance(
-                              new Date(commit?.commit.author?.date || ""),
-                              new Date(),
-                              { addSuffix: true }
-                            )}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                }}
-                selectedItemRenderer={(sha) => (
-                  <div className="flex items-center space-x-2">
-                    <CommitIcon size={16} />
-                    <div className="flex-1 truncate">
-                      <DisplayCommit
-                        message={
-                          commits.find((commit) => commit.sha === sha)?.commit
-                            .message
-                        }
-                        author={
-                          commits.find((commit) => commit.sha === sha)?.commit
-                            .author?.email
-                        }
-                        filename={filename}
-                      />
-                    </div>
+            <p className="text-xs font-medium text-indigo-200">Repository</p>
+            <div className="font-mono text-sm text-white">
+              <a
+                className="hover:underline focus:underline bg-indigo-700 hover:bg-indigo-800 focus:bg-indigo-800 h-9 rounded text-white inline-flex items-center px-2 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                target="_blank"
+                rel="noopener noreferrer"
+                href={repoUrl}
+              >
+                <div className="flex items-center space-x-2">
+                  <RepoIcon />
+                  <div
+                    className="overflow-ellipsis whitespace-nowrap overflow-hidden text-xs"
+                    style={{
+                      maxWidth: "min(30em, 100% - 1em)",
+                    }}
+                  >
+                    {owner}/{name}
                   </div>
+                  <div className="opacity-50">
+                    <LinkExternalIcon />
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          {!!(files || []).length && (
+            <div className="space-y-2">
+              <p className="text-xs font-medium text-indigo-200">Data File</p>
+              <FilePicker
+                value={filename || ""}
+                placeholder="Select a file"
+                onChange={(newFilename) => {
+                  setFilename(newFilename);
+                }}
+                items={files || []}
+                itemRenderer={(item) => (
+                  <span className="font-mono text-xs">{item}</span>
                 )}
               />
-            )}
-          </div>
-        )}
+            </div>
+          )}
 
-        {!!dataSource && (
-          <div className="space-y-2 min-w-0">
-            <p className="text-xs font-medium text-indigo-200">Data source</p>
-            <a
-              className="font-mono hover:underline focus:underline bg-indigo-700 hover:bg-indigo-800 focus:bg-indigo-800 h-9 rounded text-white flex items-center px-2 focus:outline-none focus:ring-2 focus:ring-indigo-400"
-              href={dataSource}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="flex items-center space-x-2 min-w-0">
-                <div className="flex-none">
-                  <BookmarkIcon />
+          {Boolean(filename) && (
+            <div className="space-y-2">
+              <p className="text-xs font-medium text-indigo-200">Commit</p>
+              {commits && (
+                <Picker<string>
+                  label="Choose a commit"
+                  placeholder="Select a SHA"
+                  onChange={setSelectedSha}
+                  value={selectedSha || ""}
+                  items={commits.map((commit) => commit.sha)}
+                  disclosureClass="appearance-none bg-indigo-700 hover:bg-indigo-800 focus:bg-indigo-800 h-9 px-2 rounded text-white text-xs focus:outline-none focus:ring-2 focus:ring-indigo-400 w-full lg:max-w-md"
+                  itemRenderer={(sha) => {
+                    const commit = commits.find((commit) => commit.sha === sha);
+                    return (
+                      <div className="flex flex-col space-y-1 text-xs">
+                        <DisplayCommit
+                          message={commit?.commit.message}
+                          author={commit?.commit.author?.email}
+                          filename={filename}
+                        />
+                        <div className="flex items-center space-x-2">
+                          <div className="flex items-center space-x-2">
+                            <p className="text-gray-600">
+                              {formatDistance(
+                                new Date(commit?.commit.author?.date || ""),
+                                new Date(),
+                                { addSuffix: true }
+                              )}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }}
+                  selectedItemRenderer={(sha) => (
+                    <div className="flex items-center space-x-2">
+                      <CommitIcon size={16} />
+                      <div className="flex-1 truncate">
+                        <DisplayCommit
+                          message={
+                            commits.find((commit) => commit.sha === sha)?.commit
+                              .message
+                          }
+                          author={
+                            commits.find((commit) => commit.sha === sha)?.commit
+                              .author?.email
+                          }
+                          filename={filename}
+                        />
+                      </div>
+                    </div>
+                  )}
+                />
+              )}
+            </div>
+          )}
+
+          {!!dataSource && (
+            <div className="space-y-2 min-w-0">
+              <p className="text-xs font-medium text-indigo-200">Data source</p>
+              <a
+                className="font-mono hover:underline focus:underline bg-indigo-700 hover:bg-indigo-800 focus:bg-indigo-800 h-9 rounded text-white flex items-center px-2 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+                href={dataSource}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="flex items-center space-x-2 min-w-0">
+                  <div className="flex-none">
+                    <BookmarkIcon />
+                  </div>
+                  <div className="overflow-ellipsis whitespace-nowrap overflow-hidden text-xs">
+                    {dataSource}
+                  </div>
+                  <div className="opacity-50">
+                    <LinkExternalIcon />
+                  </div>
                 </div>
-                <div className="overflow-ellipsis whitespace-nowrap overflow-hidden text-xs">
-                  {dataSource}
-                </div>
-                <div className="opacity-50">
-                  <LinkExternalIcon />
-                </div>
-              </div>
-            </a>
-          </div>
-        )}
+              </a>
+            </div>
+          )}
+        </div>
       </div>
+
       <React.Fragment>
         {selectedSha && Boolean(filename) && (
           <JSONDetail
@@ -230,13 +238,16 @@ export function RepoDetail(props: RepoDetailProps) {
           />
         )}
       </React.Fragment>
-      {match && !(files || []).length && (
-        <ErrorState img={Bug} alt="Error icon">
-          {files
-            ? "Hmm, we couldn't find any files in that repo"
-            : "Hmm, are you sure that's a public GitHub repo?"}
-        </ErrorState>
-      )}
+      {match &&
+        !(files || []).length &&
+        filesStatus !== "loading" &&
+        !filename && (
+          <ErrorState img={Bug} alt="Error icon">
+            {files
+              ? "Hmm, we couldn't find any files in that repo"
+              : "Hmm, are you sure that's a public GitHub repo?"}
+          </ErrorState>
+        )}
     </React.Fragment>
   );
 }
