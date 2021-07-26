9750bca3216fb0f59ed79ed89c4837f31956b40b
Matt Rothenberg <mattrothenberg@github.com>
2021-07-26 14:59:35 -0400

    feat: render fetched repositories

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 import { Endpoints } from "@octokit/types";
 
-export type Commit = Endpoints["GET /repos/{owner}/{repo}/commits"]["response"]["data"][0];
+export type Commit =
+  Endpoints["GET /repos/{owner}/{repo}/commits"]["response"]["data"][0];
 
 export type Repo = {
   owner: string;
@@ -11,4 +12,21 @@ export interface FlatDataTab {
   key?: string;
   value?: object[];
   invalidValue?: string;
+}
+
+interface RepositoryLicense {
+  key: string;
+  name: string;
+  url: string;
+}
+
+export interface Repository {
+  name: string;
+  description: string;
+  id: string;
+  topics?: string[];
+  stargazers_count: number;
+  language: string;
+  updated_at: string;
+  license: RepositoryLicense;
 }
